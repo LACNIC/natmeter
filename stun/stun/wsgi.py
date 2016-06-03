d1029c7962ec0af2b60e3c186d1bfddc8dd7a311
Agustín <aguformoso@gmail.com>
2016-06-03 16:26:30 -0300

    no message

--- stun/stun/wsgi.py
+++ stun/stun/wsgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/opt/django/stun/stun')
+sys.path.append('/Users/agustin/git/stun/stun/stun')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stun.settings")
 
 application = get_wsgi_application()
+
+
+
+
+
+
+
+
+
+
+
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'simon_project.settings'

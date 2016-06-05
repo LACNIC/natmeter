d33f2bded9f3b2177ce8002123a4af4fc6fa085c
Agustín <aguformoso@gmail.com>
2016-06-05 18:56:03 -0300

    Fixing CORS Headers and Django 1.9 incompatibility

--- stun/stun/urls.py
+++ stun/stun/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^stun/', include('app.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-]
+# urlpatterns = [
+#     url(r'^stun/', include('app.urls')),
+#     url(r'^admin/', include(admin.site.urls)),
+# ]
+
+
+urlpatterns = patterns('',
+                       url(r'^stun/', include('app.urls')),
+                       url(r'admin/', include(admin.site.urls)),
+                       )
+admin.autodiscover()

5d40d1f009805b71e653e5e5315424fbf89fd855
Agustín <aguformoso@gmail.com>
2016-06-05 18:37:16 -0300

    Production URLs

--- stun/stun/urls.py
+++ stun/stun/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^/', include('app.urls')),
+    url(r'^stun/', include('app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]

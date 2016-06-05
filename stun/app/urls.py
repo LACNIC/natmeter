2fd97e18550a6092be299df9252f499c9684bc37
Agustín <aguformoso@gmail.com>
2016-06-05 20:14:35 -0300

    no message

--- stun/app/urls.py
+++ stun/app/urls.py
@@ -2,12 +2,6 @@ from django.conf.urls import url, patterns
 
 from . import views
 
-# urlpatterns = [
-#     url(r'^$', views.home, name='home'),
-#     url(r'script/', views.script, name='script'),
-#     url(r'post/', views.post, name='post')
-# ]
-
 urlpatterns = patterns(
     '',
     url(r'^$', views.home, name='home'),
--- stun/stun/urls.py
+++ stun/stun/urls.py
@@ -1,14 +1,27 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-# urlpatterns = [
-#     url(r'^stun/', include('app.urls')),
-#     url(r'^admin/', include(admin.site.urls)),
-# ]
-
-
-urlpatterns = patterns('',
-                       url(r'', include('app.urls')),
-                       url(r'admin/', include(admin.site.urls)),
-                       )
 admin.autodiscover()
+
+urls = patterns('',
+                    # Common urls
+                       url(r'', include('app.urls'))
+                       # url(r'admin/', include(admin.site.urls)),
+                       )
+
+# urls = patterns('',
+#
+#                 # API urls
+#                 url(r'api/', include('simon_app.urls_api')),
+#
+#                 # Common urls
+#                 url(r'', include('simon_app.urls'))
+# )
+
+# The /simon tree root in Apache
+urlpatterns = patterns('',
+                       url(r'', include(urls)),
+
+                       # Uncomment the next line to enable the admin:
+                       url(r'admin/', include(admin.site.urls)),
+)

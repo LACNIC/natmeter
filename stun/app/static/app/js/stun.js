89e7778a74504cfb8c931f9f32b12031e817c0de
Agustín <aguformoso@gmail.com>
2016-07-18 14:38:06 +0200

    Cookies 🍪 operations

--- stun/app/static/app/js/stun.js
+++ stun/app/static/app/js/stun.js
@@ -12,6 +12,33 @@ STUN = {
 
     results: [],
 
+    COOKIES: {
+        createCookie: function (name, value, days) {
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                var expires = "; expires=" + date.toGMTString();
+            }
+            else var expires = "";
+            document.cookie = name + "=" + value + expires + "; path=/";
+        },
+
+        readCookie: function (name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+            }
+            return null;
+        },
+
+        eraseCookie: function (name) {
+            createCookie(name, "", -1);
+        }
+    },
+
     getExperimentId: function () {
         var experimentId = "", separator = "-";
         var N = 10, n = 1E6;

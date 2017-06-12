13cbf9097bbe04bf37528df6fbfead946e64c023
Agustín <aguformoso@gmail.com>
2017-06-12 14:55:43 -0300

    Export private obfuscated IP address

--- stun/app/management/commands/export_results.py
+++ stun/app/management/commands/export_results.py
@@ -11,8 +11,8 @@ class Command(BaseCommand):
         comments = []
         comments.append(["# This is a TAB separated values file containing the results gathered by the NAT Meter experiment (https://natmeter.labs.lacnic.net)."])
         comments.append(["# Private prefixes have been excluded."])
-        # comments.append(["# Local IP Addressess:  (List) Those IP addresses that the host is able to see locally"])
-        comments.append(["# Remote IP Addressess: (List) Those IP prefixes that the host is *not* able to see locally but the remote STUN server can"])
+        comments.append(["# Local IP Addressess:  (List) Those IP addresses that the host is able to see locally"])
+        comments.append(["# Remote IP Addressess: (List) Those IP prefixes that the host is *not* able to see locally but the remote STUN server is"])
         # comments.append(["# STUN Measurement ID:  The measurement ID corresponding with a certain user. This field is a cookie stored in the user's browser."])
 
         comments.append(["# The following prefixes have been excluded (LACNIC's own networks)"])
@@ -27,8 +27,7 @@ class Command(BaseCommand):
         sms = StunMeasurement.objects.all().order_by('-server_test_date')
         with open(STATIC_ROOT + '/results.csv', 'wb') as csvfile:
 
-            # fieldnames = ['local_ip_addresses', 'remote_ip_addresses', 'date']
-            fieldnames = ['remote_ip_addresses', 'date']
+            fieldnames = ['local_ip_addresses', 'remote_ip_addresses', 'date']
 
             writer = csv.writer(csvfile, delimiter='\t')
 
@@ -45,5 +44,4 @@ class Command(BaseCommand):
                 if len(stun_addresses) == 0: continue
 
                 date = sm.server_test_date.date()
-                # writer.writerow([local_addresses] + [stun_addresses] + [date])
-                writer.writerow([stun_addresses] + [date])
+                writer.writerow([local_addresses] + [stun_addresses] + [date])

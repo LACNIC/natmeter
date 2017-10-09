55b7e1575317c8c0edb690a8d58d2d75ef7e0f82
Agustín <aguformoso@gmail.com>
2017-10-09 13:03:16 -0300

    Modular STUN measurement tool

--- stun/app/static/app/js/stun.js
+++ stun/app/static/app/js/stun.js
@@ -2,305 +2,310 @@
  * Created by agustin on 5/31/16.
  */
 
-STUN = {};
-STUN.debug = false;
-STUN = {
+define(['jquery'], function() {
 
-    urls: {
-        post: STUN.debug && "http://127.0.0.1:8000/post/" || "https://natmeter.labs.lacnic.net/post/"
-    },
+  _$ = require('jquery');
 
-    callbacks: {
+  var stun = {};
+  stun.debug = false;
 
-        before_cookie: function () {
+  stun.urls = {
+    post: stun.debug && "http://127.0.0.1:8000/post/" || "https://natmeter.labs.lacnic.net/post/"
+  };
 
-        },
-        after_cookie: function () {
+  stun.callbacks = {
 
-        },
-        before_private_request: function () {
+      before_cookie: function() {
 
-        },
-        after_private_request: function () {
+      },
+      after_cookie: function() {
 
-        },
-        after_private_stun_response: function(candidate) {
+      },
+      before_private_request: function() {
 
-        },
-        before_public_request: function () {
+      },
+      after_private_request: function() {
 
-        },
-        after_public_request: function () {
+      },
+      after_private_stun_response: function(candidate) {
 
-        },
-        after_public_stun_response: function(candidate) {
+      },
+      before_public_request: function() {
 
-        },
-        before_stun_response: function () {
+      },
+      after_public_request: function() {
 
-        },
-        after_stun_response: function (line) {
+      },
+      after_public_stun_response: function(candidate) {
 
-        },
-        before_post: function () {
+      },
+      before_stun_response: function() {
 
-        },
-        after_post: function () {
+      },
+      after_stun_response: function(line) {
 
-        },
-        error_post: function () {
+      },
+      before_post: function() {
 
-        },
-        after_experiment: function () {
+      },
+      after_post: function() {
 
+      },
+      error_post: function() {
+
+      },
+      after_experiment: function() {
+
+      }
+    };
+
+    stun.iceServers = [];
+
+    stun.COOKIES = {
+
+      cookieName: "lacnic-stun-cookie",
+      cookieDays: 14,
+
+      createCookie: function(name, value, days) {
+        if (days) {
+          var date = new Date();
+          date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+          var expires = "; expires=" + date.toGMTString();
+        } else var expires = "";
+        document.cookie = name + "=" + value + expires + "; path=/";
+      },
+
+      readCookie: function(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+          if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
         }
-    },
+        return null;
+      },
 
-    iceServers: [],
+      eraseCookie: function(name) {
+        this.createCookie(name, "", -1);
+      },
 
-    COOKIES: {
+      createSTUNCookie: function(cookieValue) {
+        return this.createCookie(stun.cookieName, cookieValue, stun.cookieDays);
+      },
 
-        cookieName: "lacnic-stun-cookie",
-        cookieDays: 14,
+      readSTUNCookie: function() {
+        return this.readCookie(stun.cookieName);
+      }
+    };
 
-        createCookie: function (name, value, days) {
-            if (days) {
-                var date = new Date();
-                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-                var expires = "; expires=" + date.toGMTString();
-            }
-            else var expires = "";
-            document.cookie = name + "=" + value + expires + "; path=/";
+    stun.NETWORK = {
+      CONSTANTS: {
+        v4: 4,
+        v6: 6
+      },
+      addresses: { // IP addresses as seen externally
+        private: [],
+        public: [],
+        getPrivateAddresses: function() {
+          return stun.NETWORK.addresses.private;
         },
-
-        readCookie: function (name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-            }
-            return null;
-        },
-
-        eraseCookie: function (name) {
-            STUN.COOKIES.createCookie(name, "", -1);
-        },
-
-        createSTUNCookie: function (cookieValue) {
-            return this.createCookie(this.cookieName, cookieValue, this.cookieDays);
-        },
-
-        readSTUNCookie: function () {
-            return this.readCookie(this.cookieName);
+        getPublicAddresses: function() {
+          return stun.NETWORK.addresses.public;
         }
-    },
+      },
+      ip_address_change_event: {
+        previous: "",
+        current: ""
+      }
+    };
 
-    NETWORK: {
-        CONSTANTS: {
-            v4: 4,
-            v6: 6
+    stun.getExperimentId = function() {
+      var experimentId = "",
+        separator = "-";
+      var N = 10,
+        n = 1E6;
+      while (N > 0) {
+
+        var pos = Math.floor(Math.random() * n);
+        experimentId += pos + separator;
+        N--;
+      }
+
+      experimentId = experimentId.substring(0, experimentId.length - 1);
+      return experimentId;
+    };
+
+    stun.postResults = function() {
+
+      stun.callbacks.before_post();
+
+      const experimentId = stun.getExperimentId();
+      const cookie = stun.COOKIES.readCookie(stun.COOKIES.cookieName);
+      const data = {
+        experiment_id: experimentId,
+        cookie: cookie,
+        addresses: JSON.stringify(stun.NETWORK.addresses),
+        ip_address_change_event: JSON.stringify(stun.NETWORK.ip_address_change_event),
+        date: new Date(),
+        tester_version: 1
+      };
+
+      _$.ajax({
+        type: 'POST',
+        url: stun.urls.post,
+        data: data,
+        success: function(xml) {
+          if (xml == "OK") {
+            stun.callbacks.after_post();
+            return true;
+          }
+          return false;
         },
-        addresses: { // IP addresses as seen externally
-            private: [],
-            public: [],
-            getPrivateAddresses: function () {
-                return STUN.NETWORK.addresses.private;
-            },
-            getPublicAddresses: function () {
-                return STUN.NETWORK.addresses.public;
-            }
-        },
-        ip_address_change_event: {
-            previous: "",
-            current: ""
+        error: function(xhr, status, error) {
+          stun.callbacks.error_post();
+          return false;
         }
-    },
-
-    getExperimentId: function () {
-        var experimentId = "", separator = "-";
-        var N = 10, n = 1E6;
-        while (N > 0) {
-
-            var pos = Math.floor(Math.random() * n);
-            experimentId += pos + separator;
-            N--;
-        }
-
-        experimentId = experimentId.substring(0, experimentId.length - 1);
-        return experimentId;
-    },
-
-    postResults: function () {
-
-        STUN.callbacks.before_post();
-
-        const experimentId = STUN.getExperimentId();
-        const cookie = STUN.COOKIES.readCookie(STUN.COOKIES.cookieName);
-        const data = {
-            experiment_id: experimentId,
-            cookie: cookie,
-            addresses: JSON.stringify(STUN.NETWORK.addresses),
-            ip_address_change_event: JSON.stringify(STUN.NETWORK.ip_address_change_event),
-            date: new Date(),
-            tester_version: 1
-        };
-
-        $.ajax({
-            type: 'POST',
-            url: STUN.urls.post,
-            data: data,
-            success: function (xml) {
-                if (xml == "OK") {
-                    STUN.callbacks.after_post();
-                    return true;
-                }
-                return false;
-            },
-            error: function (xhr, status, error) {
-                STUN.callbacks.error_post();
-                return false;
-            }
-        });
-    },
+      });
+    };
 
     //get the IP addresses associated with an account
-    _init: function () {
+    stun._init = function() {
 
-        const experimentId = STUN.getExperimentId();
-        STUN.experimentId = experimentId;
-        STUN.callbacks.before_cookie();
-        if (STUN.COOKIES.readSTUNCookie() == null) {
-            STUN.COOKIES.createSTUNCookie(experimentId);
-        }
-        STUN.callbacks.after_cookie();
+      const experimentId = stun.getExperimentId();
+      stun.experimentId = experimentId;
+      stun.callbacks.before_cookie();
+      if (stun.COOKIES.readSTUNCookie() == null) {
+        stun.COOKIES.createSTUNCookie(experimentId);
+      }
+      stun.callbacks.after_cookie();
 
-        STUN.callbacks.before_stun_response();
+      stun.callbacks.before_stun_response();
 
-        //compatibility for firefox and chrome
-        var RTCPeerConnection = window.RTCPeerConnection
-            || window.mozRTCPeerConnection
-            || window.webkitRTCPeerConnection;
-        var useWebKit = !!window.webkitRTCPeerConnection;
-        //bypass naive webrtc blocking using an iframe
-        if (!RTCPeerConnection) {
-            //NOTE: you need to have an iframe in the page right above the script tag
+      //compatibility for firefox and chrome
+      var RTCPeerConnection = window.RTCPeerConnection ||
+        window.mozRTCPeerConnection ||
+        window.webkitRTCPeerConnection;
+      var useWebKit = !!window.webkitRTCPeerConnection;
+      //bypass naive webrtc blocking using an iframe
+      if (!RTCPeerConnection) {
+        //NOTE: you need to have an iframe in the page right above the script tag
 
-            var win = iframe.contentWindow;
-            RTCPeerConnection = win.RTCPeerConnection
-                || win.mozRTCPeerConnection
-                || win.webkitRTCPeerConnection;
-            useWebKit = !!win.webkitRTCPeerConnection;
-        }
-        //minimal requirements for data connection
-        var mediaConstraints = {
-            optional: [{RtpDataChannels: true}]
-        };
+        var win = iframe.contentWindow;
+        RTCPeerConnection = win.RTCPeerConnection ||
+          win.mozRTCPeerConnection ||
+          win.webkitRTCPeerConnection;
+        useWebKit = !!win.webkitRTCPeerConnection;
+      }
+      //minimal requirements for data connection
+      var mediaConstraints = {
+        optional: [{
+          RtpDataChannels: true
+        }]
+      };
 
-        //construct a new RTCPeerConnection
-        var pc = new RTCPeerConnection({iceServers: STUN.iceServers}, mediaConstraints);
+      //construct a new RTCPeerConnection
+      var pc = new RTCPeerConnection({
+        iceServers: stun.iceServers
+      }, mediaConstraints);
 
-        function handleCandidate(candidate) {
-            STUN.callbacks.after_stun_response(candidate);
-        }
+      function handleCandidate(candidate) {
+        stun.callbacks.after_stun_response(candidate);
+      }
 
-        //listen for candidate events
-        pc.onicecandidate = function (ice) {
-            //skip non-candidate events
-            if (ice.candidate)
-                handleCandidate(ice.candidate.candidate);
-        };
-        //create a bogus data channel
-        pc.createDataChannel("");
-        //create an offer sdp
-        pc.createOffer(function (result) {
-            //trigger the stun server request
-            pc.setLocalDescription(result, function () {
-            }, function () {
-            });
-        }, function () {
+      //listen for candidate events
+      pc.onicecandidate = function(ice) {
+        //skip non-candidate events
+        if (ice.candidate)
+          handleCandidate(ice.candidate.candidate);
+      };
+      //create a bogus data channel
+      pc.createDataChannel("");
+      //create an offer sdp
+      pc.createOffer(function(result) {
+        //trigger the stun server request
+        pc.setLocalDescription(result, function() {}, function() {});
+      }, function() {});
+
+      //wait for a while to let everything done
+      setTimeout(function() {
+        //read candidate info from local description
+        var lines = pc.localDescription.sdp.split('\n');
+        lines.forEach(function(line) {
+          if (line.indexOf('a=candidate:') === 0)
+            handleCandidate(line);
         });
 
-        //wait for a while to let everything done
-        setTimeout(function () {
-            //read candidate info from local description
-            var lines = pc.localDescription.sdp.split('\n');
-            lines.forEach(function (line) {
-                if (line.indexOf('a=candidate:') === 0)
-                    handleCandidate(line);
-            });
+        stun.callbacks.after_experiment();
 
-            STUN.callbacks.after_experiment();
+      }, 8000);
+    };
 
-        }, 8000);
-    },
+    stun.init = function() {
+      /*
+       * First run trying the local connection
+       */
+      stun.callbacks.after_stun_response = function(response) {
 
-    init: function () {
-        /*
-         * First run trying the local connection
-         */
-        STUN.callbacks.after_stun_response = function (response) {
+        stun.callbacks.after_private_stun_response(response);
 
-            STUN.callbacks.after_private_stun_response(response);
+        var address = response.split(" ")[4];
+        if (stun.NETWORK.addresses.private.indexOf(address) <= -1) {
+          stun.NETWORK.addresses.private.push(address);
+        }
+      }
+
+      stun.callbacks.after_experiment = function() {
+        setTimeout(function() {
+
+          /*
+           * Second run trying the remote connection (after 2 seconds...)
+           */
+          stun.callbacks.after_stun_response = function(response) {
+
+            stun.callbacks.after_public_stun_response(response);
 
             var address = response.split(" ")[4];
-            if (STUN.NETWORK.addresses.private.indexOf(address) <= -1) {
-                STUN.NETWORK.addresses.private.push(address);
+            if (stun.NETWORK.addresses.public.indexOf(address) <= -1 && stun.NETWORK.addresses.private.indexOf(address) <= -1) {
+              stun.NETWORK.addresses.public.push(address);
             }
-        }
 
-        STUN.callbacks.after_experiment = function () {
-            setTimeout(function () {
+            return true;
+          }
 
-                /*
-                 * Second run trying the remote connection (after 2 seconds...)
-                 */
-                STUN.callbacks.after_stun_response = function (response) {
+          stun.callbacks.after_experiment = function() {
+            stun.postResults();
+          }
 
-                    STUN.callbacks.after_public_stun_response(response);
+          stun.callbacks.before_public_request();
+          stun.iceServers = [{
+            urls: ["stun:stun4.acostasite.com", "stun:stun6.acostasite.com"]
+          }];
+          stun._init();
+          stun.callbacks.after_public_request();
 
-                    var address = response.split(" ")[4];
-                    if (STUN.NETWORK.addresses.public.indexOf(address) <= -1 && STUN.NETWORK.addresses.private.indexOf(address) <= -1) {
-                        STUN.NETWORK.addresses.public.push(address);
-                    }
+        }, 2000);
+      }
 
-                    return true;
-                }
+      stun.callbacks.before_private_request();
+      stun.iceServers = [];
+      stun._init();
+      stun.callbacks.after_private_request();
+    };
 
-                STUN.callbacks.after_experiment = function () {
-                    STUN.postResults();
-                }
+    stun.PARAMS = {
+      validate: function(param) {
+        return param != null && param != undefined;
+      }
+    };
 
-                STUN.callbacks.before_public_request();
-                STUN.iceServers = [
-                    {
-                        urls: ["stun:stun4.acostasite.com", "stun:stun6.acostasite.com"]
-                    }
-                ];
-                STUN._init();
-                STUN.callbacks.after_public_request();
+    stun.console = {
+      HEADER: "[STUN] : ",
+      TRAILER: " (" + new Date() + ")",
+      log: function(txt) {
+        console.log(stun.HEADER + txt + stun.TRAILER);
+      }
+    };
 
-            }, 2000);
-        }
-
-        STUN.callbacks.before_private_request();
-        STUN.iceServers = [];
-        STUN._init();
-        STUN.callbacks.after_private_request();
-    },
-
-    PARAMS: {
-        validate: function (param) {
-            return param != null && param != undefined;
-        }
-    },
-
-    console: {
-        HEADER: "[STUN] : ",
-        TRAILER: " (" + new Date() + ")",
-        log: function (txt) {
-            console.log(this.HEADER + txt + this.TRAILER);
-        }
-    }
-};
+  return stun;
+});

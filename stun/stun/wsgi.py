b9c025180ab7400da3e1c87e24b3867bcf44f0b6
Agustin Formoso <aformoso@ripe.net>
2018-07-25 16:20:29 +0200

    no message

--- stun/stun/wsgi.py
+++ stun/stun/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.append('/opt/django/stun/stun')
+sys.path.append('/opt/django/natmeter/stun')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stun.settings")
 
 from django.core.wsgi import get_wsgi_application

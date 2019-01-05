c154a6768da125818328636ee0518be31b94d96b
Agustín Formoso <aformoso@ripe.net>
2019-01-05 21:20:07 +0100

    Renaming cache import

--- stun/app/views/html/views.py
+++ stun/app/views/html/views.py
@@ -1,4 +1,4 @@
-from app.caching.caching import cache as custom_cache
+from app.caching.caching import cache as cache
 from collections import defaultdict
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
@@ -37,38 +37,38 @@ def charts(request):
     :return:
     """
 
-    v6_avg_cached = custom_cache.get(custom_cache.keys.v6_avg)['v6_count__avg']
-    v4_avg_cached = custom_cache.get(custom_cache.keys.v4_avg)['v4_count__avg']
+    v6_avg_cached = cache.get(cache.keys.v6_avg)['v6_count__avg']
+    v4_avg_cached = cache.get(cache.keys.v4_avg)['v4_count__avg']
 
     nat = rec_dd()
-    nat['all']['lac'] = custom_cache.get(custom_cache.keys.all_nat, 0)
-    nat['all']['world'] = custom_cache.get(custom_cache.keys.all_nat_world, 0)
+    nat['all']['lac'] = cache.get(cache.keys.all_nat, 0)
+    nat['all']['world'] = cache.get(cache.keys.all_nat_world, 0)
 
-    nat['v4']['lac'] = custom_cache.get(custom_cache.keys.v4_nat, 0)
-    nat['v4']['world'] = custom_cache.get(custom_cache.keys.v4_nat_world, 0)
+    nat['v4']['lac'] = cache.get(cache.keys.v4_nat, 0)
+    nat['v4']['world'] = cache.get(cache.keys.v4_nat_world, 0)
 
-    nat['v6']['lac'] = custom_cache.get(custom_cache.keys.v6_nat, 0)
-    nat['v6']['world'] = custom_cache.get(custom_cache.keys.v6_nat_world, 0)
+    nat['v6']['lac'] = cache.get(cache.keys.v6_nat, 0)
+    nat['v6']['world'] = cache.get(cache.keys.v6_nat_world, 0)
 
     v6_with_v4_cap = rec_dd()
-    v6_with_v4_cap['lac'] = custom_cache.get(custom_cache.keys.v6_with_v4_capacity)
-    v6_with_v4_cap['world'] = custom_cache.get(custom_cache.keys.v6_with_v4_capacity_world)
+    v6_with_v4_cap['lac'] = cache.get(cache.keys.v6_with_v4_capacity)
+    v6_with_v4_cap['world'] = cache.get(cache.keys.v6_with_v4_capacity_world)
 
     dualstack = rec_dd()
-    dualstack['lac'] = custom_cache.get(custom_cache.keys.dualstack)
-    dualstack['world'] = custom_cache.get(custom_cache.keys.dualstack_world)
+    dualstack['lac'] = cache.get(cache.keys.dualstack)
+    dualstack['world'] = cache.get(cache.keys.dualstack_world)
 
-    v6_only = custom_cache.get(custom_cache.keys.v6_only)
+    v6_only = cache.get(cache.keys.v6_only)
 
     npt = rec_dd()
-    npt['lac'] = custom_cache.get(custom_cache.keys.npt)
-    npt['world'] = custom_cache.get(custom_cache.keys.npt_world)
+    npt['lac'] = cache.get(cache.keys.npt)
+    npt['world'] = cache.get(cache.keys.npt_world)
 
-    country_participation_counter_cached = custom_cache.get(custom_cache.keys.country_participation)
+    country_participation_counter_cached = cache.get(cache.keys.country_participation)
 
-    public_pfxs_ratio = custom_cache.get(custom_cache.keys.public_pfxs_nat_free_0_false_percentage)
+    public_pfxs_ratio = cache.get(cache.keys.public_pfxs_nat_free_0_false_percentage)
 
-    private_prefix_counter_cached = custom_cache.get(custom_cache.keys.private_prefixes)
+    private_prefix_counter_cached = cache.get(cache.keys.private_prefixes)
 
     # Country participation chart
     total = sum(country_participation_counter_cached.values())
